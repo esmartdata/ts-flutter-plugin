c1247cbe4f0f1747ffb0ca45a880bc47caefa39e
GaozyDev <670453367@qq.com>
2023-06-21 11:34:00 +0800

    UserInfo 参数非必传

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:ts_flutter_plugin/model/init_data.dart';
 import 'package:ts_flutter_plugin/model/user_info.dart';
 import 'package:ts_flutter_plugin/ts_flutter_plugin.dart';
 
@@ -33,17 +32,7 @@ class _HomePageState extends State<HomePage> {
             ElevatedButton(
               child: const Text("用户属性"),
               onPressed: () async {
-                bool? result = await _tsFlutterPlugin.setUserInfo(UserInfo(
-                    "123456",
-                    "gzy",
-                    "gzy_nickname",
-                    "25",
-                    "2000-1-1",
-                    "男",
-                    "account_123456",
-                    "CN",
-                    "江苏",
-                    "南京"));
+                bool? result = await _tsFlutterPlugin.setUserInfo(UserInfo(guid: "123456"));
                 if (kDebugMode) {
                   print("用户属性结果:$result");
                 }
--- lib/model/user_info.dart
+++ lib/model/user_info.dart
@@ -1,43 +1,61 @@
 import 'dart:convert';
 
 class UserInfo {
+  /// 业务系统用户唯一标识
   String guid;
+
+  /// 真实姓名
   String realName;
+
+  /// 昵称
   String nickName;
+
+  /// 年龄
   String age;
+
+  /// 生日
   String birthday;
+
+  /// 性别: 男/女
   String gender;
+
+  /// 账号
   String account;
+
+  /// 国家
   String country;
+
+  /// 省份
   String province;
+
+  /// 城市
   String city;
 
   UserInfo(
-    this.guid,
-    this.realName,
-    this.nickName,
-    this.age,
-    this.birthday,
-    this.gender,
-    this.account,
-    this.country,
-    this.province,
-    this.city
-  );
+      {required this.guid,
+      this.realName = "",
+      this.nickName = "",
+      this.age = "",
+      this.birthday = "",
+      this.gender = "",
+      this.account = "",
+      this.country = "",
+      this.province = "",
+      this.city = ""});
 
   @override
   String toString() {
     return jsonEncode({
-    "guid": guid,
-    "real_name":realName,
-    "nick_name":nickName,
-    "age":age,
-    "birthday":birthday,
-    "gender":gender,
-    "account":account,
-    "country":country,
-    "province":province,
-    "city":city,
+      "guid": guid,
+      "real_name": realName,
+      "nick_name": nickName,
+      "age": age,
+      "birthday": birthday,
+      "gender": gender,
+      "account": account,
+      "country": country,
+      "province": province,
+      "city": city,
     });
   }
 }
